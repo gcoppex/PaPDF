5a83bd163b0b6fdde08e8a5d17082cb3954f5233
gcoppex <g.coppex@gmail.com>
2020-01-29 23:46:28 +0100

    Updated version to 1.0.3

--- examples/barcode.py
+++ examples/barcode.py
@@ -3,7 +3,7 @@ from PaPDF import PaPDF
 def main():
     with PaPDF("barcode.pdf") as pdf:
         pdf.addText(20, 250, "Simple EAN13 barcode example:")
-        pdf.addEAN13(27.5, 220, "0123456789128")
+        pdf.addEAN13(20, 200, "4012345123456")
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+long_description = "# PaPDF" \
+ + "Python library to create PDF" \
+ + "## Features:" \
+ + " - Basic page addition (with support of multiple formats)" \
+ + " - Basic text writing" \
+ + " - TrueType font embedding (including propper font subsetting)" \
+ + " - EAN13 barcode insertion "
+
 
 setuptools.setup(
     name="PaPdf",
-    version="1.0.1",
+    version="1.0.3",
     author="GCoppex",
     author_email="g.coppex@gmail.com",
-    description="A python package to create PDF files",
+    description="A python package to create PDF files.\nThe package" \
+        + "supports basic text edition, font embedding (TrueType) and EAN13 " \
+        + "barcode generation. Checkout https://github.com/gcoppex/PaPDF.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/gcoppex/papdf",
+    long_description_content_type='text/markdown',
+    url="https://github.com/gcoppex/PaPDF",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
